8036e7c62e70a373b3d8bad2ed5cae76999ceb39
Conrad Owen <conrad.b.owen@gmail.com>
2021-12-02 19:54:06 -0500

    Complete day 1 part 2

---
+++ day1/solution-part2.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+// Quick definition for cleaner sums later
+const sum = (array) => array.reduce((a, b) => a + b);
+
+var filedata;
+try {
+  filedata = fs.readFileSync(`${__dirname}/input.txt`, "utf8");
+} catch (err) {
+  console.error(err);
+}
+
+var depths = filedata.toString().trim().split("\n").map(Number);
+
+// console.log(depths);
+
+var totalIncreases = 0;
+
+// sliding windows of triples this time
+// start from index 1 instead of 0 so we have something to compare against
+// Array.slice is end-exclusive, so we have to go 1 beyond array length
+for (var i = 3; i < depths.length; i++) {
+  const prevTripleSum = sum(depths.slice(i - 3, i));
+  const currTripleSum = sum(depths.slice(i - 2, i + 1));
+
+  if (prevTripleSum < currTripleSum) {
+    totalIncreases++;
+  }
+}
+
+console.log("Total increases: " + totalIncreases);
